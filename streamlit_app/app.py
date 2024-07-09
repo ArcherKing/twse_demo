92272e082b73609c5ac09e7b683d147733b81077
ArcherKing <s10243007@gmail.com>
2024-07-09 20:17:29 +0800

    優化資料載入及 UI 體驗

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -3,9 +3,8 @@ import pandas as pd
 import altair as alt
 
 import requests
-import json
 import time
-
+from datetime import datetime, timedelta
 
 message_1 = r"""
 嗨，我是Archer👋
@@ -36,7 +35,7 @@ B. 作業流程主要分為以下2個部分：
 
     2. 網頁資料視覺化呈現
 
-        a. 於 Streamlit Community Cloud 執行 (位於美國，可能需要一些時間等待一下)
+        a. 於 Streamlit Community Cloud 執行 (位於美國)
         b. 使用者在 STOCK 頁籤
         c. 呼叫 Azure Functions API函數取得所有股票
         d. 使用者選取欲查看之股票
@@ -47,23 +46,11 @@ message_2 = """
 感謝您撥冗閱覽，祝您有愉快的一天🤗
 """
 
-get_stock_list_api = (
-    "https://archer-twse-functions.azurewebsites.net/api/get_stock_list?"
-)
+STOCK_LIST_API = "https://archer-twse-functions.azurewebsites.net/api/get_stock_list?"
 
-get_stock_daily_api = (
+STOCK_DAILY_API = (
     "https://archer-twse-functions.azurewebsites.net/api/http_trigger?stock={stock}"
 )
-
-
-def get_stock_daily_price(stock: str):
-    response = requests.post(get_stock_daily_api.format(stock=stock))
-    return json.loads(response.text)
-
-
-def get_stock_list():
-    response = requests.post(get_stock_list_api)
-    return json.loads(response.text)
 
 
 def stream_data(message: str):
@@ -72,13 +59,216 @@ def stream_data(message: str):
         time.sleep(0.03)
 
 
-def main():
-    #
-    stock_list = get_stock_list()
-    selectbox_options = [" ".join(stock.values()) for stock in stock_list]
+def get_ttl():
+    now = datetime.now()
+    next_time_1430 = now.replace(hour=6, minute=30, second=59, microsecond=0)
+    if now > next_time_1430:
+        next_time_1430 += timedelta(days=1)
+    return next_time_1430 - now
 
-    #
+
+@st.cache_data(ttl=get_ttl())
+def get_stock_list() -> list:
+    """取得股票清單"""
+    try:
+        response = requests.post(STOCK_LIST_API)
+        response.raise_for_status()
+        stock_list = [" ".join(stock.values()) for stock in response.json()]
+        return stock_list
+    except requests.RequestException as e:
+        st.error(f"Error fetching stock list: {e}")
+        return []
+
+
+@st.cache_data(ttl=get_ttl())
+def get_stock_daily_price(stock: str) -> pd.DataFrame:
+    """取得個股每日交易資訊"""
+    try:
+        response = requests.post(STOCK_DAILY_API.format(stock=stock))
+        response.raise_for_status()
+        stock_daily_data = response.json()
+        return _clean_data_and_to_df(stock_daily_data)
+    except requests.RequestException as e:
+        st.error(f"Error fetching daily stock price: {e}")
+        return pd.DataFrame()
+
+
+def _clean_data_and_to_df(stock_daily) -> pd.DataFrame:
+    """清理資料並轉為DataFrame"""
+    stock_daily_df = (
+        pd.DataFrame(stock_daily)
+        .replace("None", pd.NA)
+        .dropna()
+        .assign(trade_date=lambda df: pd.to_datetime(df["trade_date"]))
+    )
+
+    filter_col = [
+        "opening_price",
+        "highest_price",
+        "lowest_price",
+        "closing_price",
+    ]
+    stock_daily_df = stock_daily_df[
+        ~(stock_daily_df[filter_col] == "$0.00").any(axis=1)
+    ]
+
+    columns_to_float = [
+        "trade_value",
+        "opening_price",
+        "highest_price",
+        "lowest_price",
+        "closing_price",
+        "change",
+    ]
+    stock_daily_df[columns_to_float] = (
+        stock_daily_df[columns_to_float].replace({"[$,]": ""}, regex=True).astype(float)
+    )
+    columns_to_int = [
+        "trade_volume",
+        "transaction",
+    ]
+    stock_daily_df[columns_to_int] = stock_daily_df[columns_to_int].astype(int)
+    return stock_daily_df
+
+
+@st.experimental_fragment
+def fragment_stock() -> None:
+    """根據選項獨立重新運作區塊"""
+
+    start_time = datetime.now()
+
+    selectbox_options = get_stock_list()
+    selected_stock = st.selectbox(
+        "Stock", options=selectbox_options, placeholder="Select Stock..."
+    )
+    if selected_stock:
+        stock = dict(zip(["code", "name"], selected_stock.split(" ")))
+        stock_daily_df = get_stock_daily_price(stock.get("code"))
+
+        if not stock_daily_df.empty:
+            display_stock_info(stock_daily_df, stock)
+
+    end_time = datetime.now()
+    st.toast(f"Finished in {(end_time-start_time).total_seconds()} s", icon="✅")
+
+
+def display_stock_info(stock_daily_df, stock) -> None:
+    """Display the stock information in the Streamlit app."""
+    today_data = stock_daily_df.iloc[-1]
+    yesterday_data = stock_daily_df.iloc[-2]
+
+    col1, col2, col3 = st.columns(3)
+    col1.metric(
+        "日期", today_data["trade_date"].strftime("%Y/%m/%d"), delta_color="off"
+    )
+    col2.metric(
+        "股價", today_data["closing_price"], today_data["change"], delta_color="inverse"
+    )
+    col3.metric(
+        "成交量(股)",
+        f'{int(today_data["trade_volume"]):,}',
+        f'{int(today_data["trade_volume"]) - int(yesterday_data["trade_volume"]):,}',
+        delta_color="inverse",
+    )
+
+    input_date_long = 7
+    date_long = min(stock_daily_df.shape[0], input_date_long)
+    df_long_data = stock_daily_df.iloc[-1 : -date_long - 1 : -1].copy()
+    df_long_data["trade_date"] = df_long_data["trade_date"].apply(
+        lambda d: d.date().strftime("%Y-%m-%d")
+    )
+    df_long_data["code"] = stock["code"]
+    df_long_data["name"] = stock["name"]
+
+    st.dataframe(
+        df_long_data[
+            [
+                "trade_date",
+                "code",
+                "name",
+                "opening_price",
+                "highest_price",
+                "lowest_price",
+                "closing_price",
+                "trade_volume",
+            ]
+        ],
+        column_config={
+            "trade_date": st.column_config.TextColumn("交易日期"),
+            "code": st.column_config.TextColumn("代號"),
+            "name": st.column_config.TextColumn("名稱"),
+            "opening_price": st.column_config.NumberColumn("開盤價"),
+            "highest_price": st.column_config.NumberColumn("最高價"),
+            "lowest_price": st.column_config.NumberColumn("最低價"),
+            "closing_price": st.column_config.NumberColumn("收盤價"),
+            "trade_volume": st.column_config.NumberColumn("成交量(股)"),
+        },
+        hide_index=True,
+        use_container_width=True,
+    )
+
+    df_line = pd.DataFrame(
+        {
+            "views_history_7": [
+                stock_daily_df["closing_price"].tail(7).values.tolist()
+            ],
+            "views_history_30": [
+                stock_daily_df["closing_price"].tail(30).values.tolist()
+            ],
+            "views_history_365": [
+                stock_daily_df["closing_price"].tail(365).values.tolist()
+            ],
+        }
+    )
+
+    st.dataframe(
+        df_line,
+        column_config={
+            "views_history_7": st.column_config.LineChartColumn(
+                "週趨勢",
+                y_min=float(min(df_line["views_history_7"].values[0])),
+                y_max=float(max(df_line["views_history_7"].values[0])),
+            ),
+            "views_history_30": st.column_config.LineChartColumn(
+                "月趨勢",
+                y_min=float(min(df_line["views_history_30"].values[0])),
+                y_max=float(max(df_line["views_history_30"].values[0])),
+            ),
+            "views_history_365": st.column_config.LineChartColumn(
+                "年趨勢",
+                y_min=float(min(df_line["views_history_365"].values[0])),
+                y_max=float(max(df_line["views_history_365"].values[0])),
+            ),
+        },
+        hide_index=True,
+        use_container_width=True,
+    )
+
+    cp_chart = (
+        alt.Chart(stock_daily_df)
+        .mark_line(clip=True)
+        .encode(
+            alt.X("trade_date").title("交易日期"), alt.Y("closing_price").title("股價")
+        )
+        .interactive(bind_y=False)
+    )
+    st.altair_chart(cp_chart, theme="streamlit", use_container_width=True)
+
+    tv_chart = (
+        alt.Chart(stock_daily_df)
+        .mark_line(clip=True)
+        .encode(
+            alt.X("trade_date").title("交易日期"),
+            alt.Y("trade_volume").title("成交量(股)"),
+        )
+        .interactive(bind_y=False)
+    )
+    st.altair_chart(tv_chart, theme="streamlit", use_container_width=True)
+
+
+def main():
     st.title("Archer TWSE Demo")
+    st.info("[2024.07.09]最近更新: 優化資料載入及 UI 體驗，減少等待時間。 ", icon="ℹ️")
     tab1, tab2 = st.tabs(["HOME", "STOCK"])
     with tab1:
         st.subheader("Hello")
@@ -104,188 +294,7 @@ def main():
         st.image("streamlit_app/azure.jpg", caption="Azure")
 
     with tab2:
-        seleted_stock = st.selectbox(
-            "Stock",
-            options=selectbox_options,
-            index=None,
-            placeholder="Select Stock...",
-        )
-
-        if seleted_stock:
-            seleted_stock = dict(zip(["code", "name"], seleted_stock.split(" ")))
-            stock_daily = get_stock_daily_price(seleted_stock.get("code"))
-
-            # data load and process
-            stock_daily_df = (
-                pd.DataFrame(stock_daily)
-                .replace("None", pd.NA)
-                .dropna()
-                .assign(trade_date=lambda df: pd.to_datetime(df["trade_date"]))
-            )
-
-            filter_col = [
-                "opening_price",
-                "highest_price",
-                "lowest_price",
-                "closing_price",
-            ]
-            stock_daily_df = stock_daily_df[
-                ~(stock_daily_df[filter_col] == "$0.00").any(axis=1)
-            ]
-
-            columns_to_float = [
-                "trade_value",
-                "opening_price",
-                "highest_price",
-                "lowest_price",
-                "closing_price",
-                "change",
-            ]
-            stock_daily_df[columns_to_float] = (
-                stock_daily_df[columns_to_float]
-                .replace({"[$,]": ""}, regex=True)
-                .astype(float)
-            )
-            columns_to_int = [
-                "trade_volume",
-                "transaction",
-            ]
-            stock_daily_df[columns_to_int] = stock_daily_df[columns_to_int].astype(int)
-
-            # 最後交易日簡易資訊
-            today_data = stock_daily_df.iloc[-1]
-            yesterday_data = stock_daily_df.iloc[-2]
-            col1, col2, col3 = st.columns(3)
-            col1.metric(
-                "日期",
-                today_data["trade_date"].strftime("%Y/%m/%d"),
-                delta_color="off",
-            )
-            col2.metric(
-                "股價",
-                today_data["closing_price"],
-                today_data["change"],
-                delta_color="inverse",
-            )
-            col3.metric(
-                "成交量(股)",
-                f'{int(today_data["trade_volume"]):,}',
-                f'{int(today_data["trade_volume"])-int(yesterday_data["trade_volume"]):,}',
-                delta_color="inverse",
-            )
-
-            # 近一週資訊表格
-            date_long = min(stock_daily_df.shape[0], 7)
-            df_long_data = pd.DataFrame(
-                {
-                    "trade_date": stock_daily_df.loc[
-                        stock_daily_df.index[-1 : date_long * (-1) - 1 : -1],
-                        "trade_date",
-                    ].apply(lambda d: str(d.date())),
-                    "code": [seleted_stock.get("code")] * date_long,
-                    "name": [seleted_stock.get("name")] * date_long,
-                    "opening_price": stock_daily_df.loc[
-                        stock_daily_df.index[-1 : date_long * (-1) - 1 : -1],
-                        "opening_price",
-                    ],
-                    "highest_price": stock_daily_df.loc[
-                        stock_daily_df.index[-1 : date_long * (-1) - 1 : -1],
-                        "highest_price",
-                    ],
-                    "lowest_price": stock_daily_df.loc[
-                        stock_daily_df.index[-1 : date_long * (-1) - 1 : -1],
-                        "lowest_price",
-                    ],
-                    "closing_price": stock_daily_df.loc[
-                        stock_daily_df.index[-1 : date_long * (-1) - 1 : -1],
-                        "closing_price",
-                    ],
-                    "trade_volume": stock_daily_df.loc[
-                        stock_daily_df.index[-1 : date_long * (-1) - 1 : -1],
-                        "trade_volume",
-                    ],
-                }
-            )
-            st.dataframe(
-                df_long_data,
-                column_config={
-                    "trade_date": st.column_config.TextColumn("交易日期"),
-                    "code": st.column_config.TextColumn("代號"),
-                    "name": st.column_config.TextColumn("名稱"),
-                    "opening_price": st.column_config.NumberColumn("開盤價"),
-                    "highest_price": st.column_config.NumberColumn("最高價"),
-                    "lowest_price": st.column_config.NumberColumn("最低價"),
-                    "closing_price": st.column_config.NumberColumn("收盤價"),
-                    "trade_volume": st.column_config.NumberColumn("成交量(股)"),
-                },
-                hide_index=True,
-                use_container_width=True,
-            )
-
-            # 週月年趨勢
-            df_line = pd.DataFrame(
-                {
-                    "views_history_7": [
-                        stock_daily_df.loc[
-                            stock_daily_df.index[-7:], "closing_price"
-                        ].T.values.tolist()
-                    ],
-                    "views_history_30": [
-                        stock_daily_df.loc[
-                            stock_daily_df.index[-30:], "closing_price"
-                        ].T.values.tolist()
-                    ],
-                    "views_history_365": [
-                        stock_daily_df.loc[
-                            stock_daily_df.index[-365:], "closing_price"
-                        ].T.values.tolist()
-                    ],
-                }
-            )
-            st.dataframe(
-                df_line,
-                column_config={
-                    "views_history_7": st.column_config.LineChartColumn(
-                        "週趨勢",
-                        y_min=float(min(df_line["views_history_7"].values[0])),
-                        y_max=float(max(df_line["views_history_7"].values[0])),
-                    ),
-                    "views_history_30": st.column_config.LineChartColumn(
-                        "月趨勢",
-                        y_min=float(min(df_line["views_history_30"].values[0])),
-                        y_max=float(max(df_line["views_history_30"].values[0])),
-                    ),
-                    "views_history_365": st.column_config.LineChartColumn(
-                        "年趨勢",
-                        y_min=float(min(df_line["views_history_365"].values[0])),
-                        y_max=float(max(df_line["views_history_365"].values[0])),
-                    ),
-                },
-                hide_index=True,
-                use_container_width=True,
-            )
-
-            # 歷史趨勢
-            cp_chart = (
-                alt.Chart(stock_daily_df)
-                .mark_line(clip=True)
-                .encode(
-                    alt.X("trade_date").title("交易日期"),
-                    alt.Y("closing_price").title("股價"),
-                )
-                .interactive(bind_y=False)
-            )
-            st.altair_chart(cp_chart, theme="streamlit", use_container_width=True)
-            tv_chart = (
-                alt.Chart(stock_daily_df)
-                .mark_line(clip=True)
-                .encode(
-                    alt.X("trade_date").title("交易日期"),
-                    alt.Y("trade_volume").title("成交量(股)"),
-                )
-                .interactive(bind_y=False)
-            )
-            st.altair_chart(tv_chart, theme="streamlit", use_container_width=True)
+        fragment_stock()
 
 
 if __name__ == "__main__":
